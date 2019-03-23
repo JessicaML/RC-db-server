16b22f988f90ba71c30a9e5f078af13adccfb306
Jessica Leach <jessica@info.nl>
2019-03-23 20:56:19 +0100

    sends data from params, not body

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,6 +1,8 @@
 const ObjectID = require("mongodb").ObjectID;
 module.exports = function(app, db) {
   app.get("/notes/:id", (req, res) => {
+    //http://localhost:4000/set?:key=:value
+    //http://localhost:4000/set?somekey=somevalue
     const id = req.params.id;
     const details = { _id: new ObjectID(id) };
     db.collection("notes").findOne(details, (err, item) => {
@@ -11,8 +13,14 @@ module.exports = function(app, db) {
       }
     });
   });
-  app.post("/notes", (req, res) => {
-    const note = { text: req.body.body, title: req.body.title };
+  app.post("/set/:n/:f", (req, res) => {
+    const note = { text: req.params.n, title: req.params.f };
+    const n = req.params.n;
+    const f = req.params.f;
+    console.log(f);
+
+    console.log(n);
+    console.log(note);
     db.collection("notes").insert(note, (err, result) => {
       if (err) {
         res.send({ error: "An error has occurred" });
